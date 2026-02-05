7266aa3ee9cfc3ed4de5c302aa00f9ae80ecac6d
SMaksimqa <liveestar24@gmail.com>
2026-02-06 01:55:13 +0300

    Create teachers-for-math.sql

---
+++ sql-examples/teachers-for-math.sql
@@ -0,0 +1,11 @@
+-- teachers-for-math.sql
+-- Учителя, которые ведут математику (JOIN + фильтр по предмету)
+
+SELECT DISTINCT
+    t.first_name,
+    t.last_name
+FROM Teacher t
+INNER JOIN Schedule sc ON t.id = sc.teacher
+INNER JOIN Subject sub ON sc.subject = sub.id
+WHERE sub.name = 'Math'
+ORDER BY t.last_name;
